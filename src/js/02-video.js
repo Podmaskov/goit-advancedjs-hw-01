9a0b6f8cca9bc71dabbda591944dc9a4562fe3ed
Podmaskov <podmaskov@gmail.com>
2023-11-06 15:53:11 +0100

    player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const THROTTLE_TIME = 1000;
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const throttledLocalStorage = throttle(
+  seconds => {
+    localStorage.setItem('videoplayer-current-time', seconds);
+  },
+  THROTTLE_TIME,
+  { leading: false }
+);
+
+player.on('timeupdate', ({ seconds }) => {
+  throttledLocalStorage(seconds);
+});
+
+const currentVideoTime = localStorage.getItem('videoplayer-current-time') || 0;
+player.setCurrentTime(currentVideoTime);

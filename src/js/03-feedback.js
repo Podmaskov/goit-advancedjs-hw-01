32ba17bc73466c8b62b08b29a6f5d52fd223b1f4
Podmaskov <podmaskov@gmail.com>
2023-11-06 17:28:04 +0100

    form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
+import throttle from 'lodash.throttle';
+const THROTTLE_TIME = 500;
+const LS_KEY = 'feedback-form-state';
+const feedbackForm = document.querySelector('.feedback-form');
 
+(function preFillForm() {
+  const localStorageData =
+    localStorage.getItem(LS_KEY) && JSON.parse(localStorage.getItem(LS_KEY));
+
+  if (!localStorageData) return;
+
+  const inputEmail = feedbackForm.querySelector('input');
+  const textareaMessage = feedbackForm.querySelector('textarea');
+  inputEmail.value = localStorageData.email;
+  textareaMessage.value = localStorageData.message;
+})();
+
+function setDataToLocalStorage({ target }) {
+  const localStorageData =
+    localStorage.getItem(LS_KEY) && JSON.parse(localStorage.getItem(LS_KEY));
+
+  if (localStorageData) {
+    const formData = JSON.stringify({
+      ...localStorageData,
+      [target.name]: target.value,
+    });
+
+    localStorage.setItem(LS_KEY, formData);
+  } else {
+    localStorage.setItem(
+      LS_KEY,
+      JSON.stringify({ [target.name]: target.value })
+    );
+  }
+}
+
+function onSubmit(event) {
+  event.preventDefault();
+  feedbackForm.reset();
+  console.log(JSON.parse(localStorage.getItem(LS_KEY)));
+  localStorage.removeItem(LS_KEY);
+}
+
+feedbackForm.addEventListener(
+  'input',
+  throttle(setDataToLocalStorage, THROTTLE_TIME)
+);
+
+feedbackForm.addEventListener('submit', onSubmit);
